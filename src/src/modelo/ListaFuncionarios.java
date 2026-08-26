cee8be62fe1fbc00a9e2d3cb6e4e9829c3d3dd3d
isaachdzv13-crypto <isaachdzv13@gmail.com>
2026-08-25 18:48:19 -0600

    funcionariosLista

--- src/src/modelo/ListaFuncionarios.java
+++ src/src/modelo/ListaFuncionarios.java
@@ -1,4 +1,40 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class ListaFuncionarios {
+    private List<Funcionario> listaFuncionarios;
+
+    ListaFuncionarios(){
+        listaFuncionarios= new ArrayList<>();
+    }
+    public void addRecurso(Funcionario func) {
+        listaFuncionarios.add(func);
+    }
+
+    public boolean borrarRecurso(String id) {
+
+        Funcionario funcionario = buscarPorId(id);
+
+        if (funcionario != null) {
+            listaFuncionarios.remove(funcionario);
+            return true;
+        }
+
+        return false;
+    }
+    public Funcionario buscarPorId(String id) {
+
+        for (Funcionario r : listaFuncionarios) {
+
+            if (Objects.equals(r.getId(), id)) {
+                return r;
+            }
+        }
+
+        return null;
+    }
+
 }
