e721548be5586e929a9f826b5da1ab91dcdbe191
isaachdzv13-crypto <isaachdzv13@gmail.com>
2026-08-25 19:26:50 -0600

    reservaLista

--- src/src/modelo/ListaReservas.java
+++ src/src/modelo/ListaReservas.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListaReservas {
     private List<Reserva> listaReservas;
@@ -9,5 +10,19 @@ public class ListaReservas {
         listaReservas= new ArrayList<>();
     }
 
+    public void add(Reserva r){
+        listaReservas.add(r);
+    }
 
+public ListaReservas reservasPorId(String id){
+        ListaReservas rs = new ListaReservas();
+        for(Reserva s: this.listaReservas){
+            if(Objects.equals(s.getIdFuncionario(), id)){
+                rs.add(s);
+            }
+        }
+
+        return rs;
+
+}
 }
